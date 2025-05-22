1ac06745ea8f978daa0a22c2b07da59a39382d74
Yessica Alexandra Conejo Muñoz <munozyessica2001@gmail.com>
2025-05-22 14:53:38 -0500

    Registrar rutas de pagos y medios de pago en app principal

--- backend-reservas/app.js
+++ backend-reservas/app.js
@@ -18,12 +18,14 @@ require('./models');
 const authRoutes = require('./routes/authRoutes');
 const servicioRoutes = require('./routes/servicioRoutes');
 const citaRoutes = require('./routes/citaRoutes');
-
+const pagoRoutes = require('./routes/pago');
+const medioPagoRoutes = require('./routes/medio_pago');
 // Usar rutas
 app.use('/api/auth', authRoutes);
 app.use('/api/servicios', servicioRoutes);
 app.use('/api/citas', citaRoutes);
-
+app.use('/api/pagos', pagoRoutes);
+app.use('/api/medios_pago', medioPagoRoutes);
 // Puerto
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
