c9e90ac5c1b0c53ba698640ef215101625d83c5a
Yessica Alexandra Conejo Muñoz <munozyessica2001@gmail.com>
2025-05-21 19:19:48 -0500

    Crear middleware para validar token JWT y proteger rutas

---
+++ backend-reservas/middlewares/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = 'tu_clave_secreta_super_segura';
+
+function verificarToken(req, res, next) {
+  const token = req.headers['authorization'];
+
+  if (!token) return res.status(403).json({ mensaje: 'Token requerido' });
+
+  try {
+    const verificado = jwt.verify(token.split(' ')[1], SECRET_KEY);
+    req.usuario = verificado;
+    next();
+  } catch (error) {
+    res.status(401).json({ mensaje: 'Token inválido o expirado' });
+  }
+}
+
+module.exports = verificarToken;
