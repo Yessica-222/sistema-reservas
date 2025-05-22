b5c4afef9b7bb20a7299bd8aa9511e96d8c02962
Yessica Alexandra Conejo Muñoz <munozyessica2001@gmail.com>
2025-05-21 21:13:25 -0500

    Se agrega la ruta protegida para servicio con middleware

---
+++ backend-reservas/routes/servicioRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const verificarToken = require('../middleware/verificarToken');
+const servicioController = require('../controllers/servicioController');
+
+router.get('/', servicioController.obtenerServicios);
+router.post('/', verificarToken, servicioController.crearServicio);
+router.put('/:id', verificarToken, servicioController.actualizarServicio);
+router.delete('/:id', verificarToken, servicioController.eliminarServicio);
+
+module.exports = router;
