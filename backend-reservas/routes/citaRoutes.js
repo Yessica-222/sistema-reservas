c0d84d597b02dfe42910f1b426e4b287565ee394
Yessica Alexandra Conejo Muñoz <munozyessica2001@gmail.com>
2025-05-21 21:09:42 -0500

    Se crean las rutas para citas

---
+++ backend-reservas/routes/citaRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const verificarToken = require('../middleware/verificarToken');
+const citaController = require('../controllers/citaController');
+
+router.get('/', verificarToken, citaController.obtenerCitas);
+router.post('/', verificarToken, citaController.crearCita);
+router.put('/:id', verificarToken, citaController.actualizarCita);
+router.delete('/:id', verificarToken, citaController.eliminarCita);
+
+module.exports = router;
